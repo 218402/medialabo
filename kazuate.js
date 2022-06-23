db73a6fd4ed0fb64ec739bddeb0f6012f5ba2ff7
G184022021 <218402@st.takushoku-u.ac.jp>
2022-06-23 14:43:25 +0900

    第三回授業の成果

--- kazuate.js
+++ kazuate.js
@@ -6,6 +6,7 @@ console.log('答え: ' + kotae);      // デバッグ用
 let kaisu = 0;
 
 // 予想を4回実行する
+
 // 将来: ボタンを押したら， hantei() を呼び出すように修正する
 hantei();
 hantei();
@@ -15,8 +16,18 @@ hantei();
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
     let yoso = 4;       // 第5回課題:テキストボックスの数値をここに代入
+    kaisu++;
+    console.log(kaisu+"回目の予想："+yoso)
+    
+    if(yoso===kotae){
+        console.log("正解です．おめでとう!");
+    }else if(yoso>kotae){
+        console.log("まちがい．答えはもっと大きいですよ");
+    }else if(yoso<kotae){
+        console.log("まちがい．答えはもっと小さいですよ");
+    }
     // 課題3-1：ここの判定処理を作成する．
     //        ページに表示する方法はまだ習っていないので
     //        判定結果はコンソールに出力すること
-
 }
+console.log("答えは"+kotae+"でした．すでにゲームは終わっています");
